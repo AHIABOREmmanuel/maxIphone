00a2bdb61fa86a4b5a058de967d7edc2f03b074d
Emmanuel ahiabor <ahiaboremmanuel9@gmail.com>
2025-01-21 09:36:34 +0000

    first commit

---
+++ src/app/body/body.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-body',
+  templateUrl: './body.component.html',
+  styleUrl: './body.component.css'
+})
+export class BodyComponent {
+  cards = [
+    { title: 'Réparations Rapides', text: 'Des réparations garanties et rapides.', image: 'assets/deal.jpeg' },
+    { title: 'iPhones en Vente', text: 'Les meilleures offres pour des iPhones.', image: 'assets/deal.jpeg' },
+    { title: 'Deals Exclusifs', text: 'Des deals qui dépassent vos attentes.', image: 'assets/deal.jpeg' },
+  ];
+  
+  
+}
