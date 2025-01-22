b5d59d5dff4a4392344fb6651e139d7a54396660
Emmanuel ahiabor <ahiaboremmanuel9@gmail.com>
2025-01-22 04:31:43 +0000

    modification

--- src/app/body/body.component.ts
+++ src/app/body/body.component.ts
@@ -6,11 +6,28 @@ import { Component } from '@angular/core';
   styleUrl: './body.component.css'
 })
 export class BodyComponent {
+  
   cards = [
-    { title: 'Réparations Rapides', text: 'Des réparations garanties et rapides.', image: 'assets/deal.jpeg' },
-    { title: 'iPhones en Vente', text: 'Les meilleures offres pour des iPhones.', image: 'assets/deal.jpeg' },
-    { title: 'Deals Exclusifs', text: 'Des deals qui dépassent vos attentes.', image: 'assets/deal.jpeg' },
+    { 
+      title: 'Réparations Rapides', 
+      text: 'Des réparations garanties et rapides.', 
+      image: 'assets/deal.jpeg',
+      discount: '10%'  // Ajout du champ pourcentage de réduction
+    },
+    { 
+      title: 'iPhones en Vente', 
+      text: 'Les meilleures offres pour des iPhones.', 
+      image: 'assets/newip.jpeg',
+      discount: '15%'  // Ajout du champ pourcentage de réduction
+    },
+    { 
+      title: 'Deals Exclusifs', 
+      text: 'Des deals qui dépassent vos attentes.', 
+      image: 'assets/phone16.jpeg',
+      discount: '20%'  // Ajout du champ pourcentage de réduction
+    },
   ];
+  
   
   
 }
