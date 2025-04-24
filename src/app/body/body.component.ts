196e5d0464cff470e86c5df47a22cf92adfb3ae1
Emmanuel ahiabor <ahiaboremmanuel9@gmail.com>
2025-04-24 12:57:35 +0000

    24/04/2025 pourcentage de réduction

--- src/app/body/body.component.ts
+++ src/app/body/body.component.ts
@@ -12,19 +12,19 @@ export class BodyComponent {
       title: 'Réparations Rapides',
       text: 'Des réparations garanties et rapides.',
       image: 'assets/deal.jpeg',
-      discount: '10%' 
+      discount: '10%'
     },
     {
       title: 'iPhones en Vente',
       text: 'Les meilleures offres pour des iPhones.',
       image: 'assets/deal.jpeg',
-      discount: '15%'
+      discount: '10%'
     },
     {
       title: 'Deals Exclusifs',
       text: 'Des deals qui dépassent vos attentes.',
       image: 'assets/deal.jpeg',
-      discount: '20%'
+      discount: '10%'
     },
   ];
 
